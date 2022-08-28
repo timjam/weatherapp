87fb7854f71d0c66b04794e49e28f1829c33f0cf
Timo Mätäsaho <timo.matasaho@gmail.com>
2022-08-28 22:16:31 +0300

    Some docs and postman collection for testing

--- README.md
+++ README.md
@@ -17,3 +17,48 @@ If you want seed some data before using the system at the backend directory type
 ```sh
 yarn knex:seed
 ```
+
+
+## API
+
+Quick reference to the API
+
+For local testing you can use the provided Postman collection [Weather app.postman_collection.js](./Weather%20App.postman_collection.json)
+
+### GET {baseUrl}/weather
+
+Returns all hits from the weatherdata table
+
+### GET {baseUrl}/weather/location
+
+Returns the data from the closest location to the given longitude and latitude.
+
+```
+query params:
+   format - either C or F. Returns temperatures either in Celsius (default) or Fahrenheit
+   limit - positive integer. Returns n closest hits to given longitude and latitude. Defaults to 1
+
+body:
+  {
+   longitude - Location longitude in format 'xx.xxxx'
+   latitude - Location latitude in format 'xx.xxxx'
+  }
+```
+
+### POST {baseUrl}/weather/location
+
+Insert new locations with temperature data. Request body needs to be an array of temperature data
+
+```
+body:
+   [
+     {
+       "city": "City name",
+       "lat": "60.1234",
+       "lon": "22.1234",
+       "temp": "-14.5",
+       "humidity": "40.3",
+     },
+     ...
+   ]
+```
--- backend/src/api/weather.ts
+++ backend/src/api/weather.ts
@@ -5,11 +5,31 @@ import { validateRequest } from "zod-express-middleware"
 
 export const weatherRouter = Router()
 
+/**
+ * GET {baseUrl}/weather
+ * 
+ * Returns all hits from the weatherdata table
+ */
 weatherRouter.get("/", async (req, res, next) => {
   const results = await getAll().catch(error => error)
   return res.send(results)
 })
 
+/**
+ * GET {baseUrl}/weather/location
+ * 
+ * Returns the data from the closest location to the given
+ * longitude and latitude.
+ * 
+ * query params:
+ *    format - either C or F. Returns temperatures either in Celsius (default) or Fahrenheit
+ *    limit - positive integer. Returns n closest hits to given longitude and latitude. Defaults to 1
+ * 
+ * body:
+ *    longitude - Location longitude in format 'xx.xxxx'
+ *    latitude - Location latitude in format 'xx.xxxx'
+ * 
+ */
 weatherRouter.get("/location",
   validateRequest(LocationQueryCodec),
   async (req, res, next) => {
@@ -20,6 +40,25 @@ weatherRouter.get("/location",
     return res.send(results)
 })
 
+/**
+ * POST {baseUrl}/weather/location
+ * 
+ * Insert new locations with temperature data. Request body needs to
+ * be an array of temperature data
+ * 
+ * body:
+ *    [
+ *      {
+ *        "city": "City name",
+ *        "lat": "xx.xxxx",
+ *        "lon": "xx.xxxx",
+ *        "temp": "xxxx.xxxx",
+ *        "humidity": "xxxx.xxxx",
+ *      },
+ *      ...
+ *    ]
+ * 
+ */
 weatherRouter.post("/location",
   validateRequest(LocationInputCodec),
   async (req, res, next) => {
